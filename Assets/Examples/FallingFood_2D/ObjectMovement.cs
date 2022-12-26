d4ad49e02b0d577414a8cf5a91e861cd2a7cdb74
Mikhail M <mayurovmih@gmail.com>
2022-12-26 15:50:03 +0300

    Реализовал падение объектов с постоянной скоростью.

--- Assets/Examples/FallingFood_2D/ObjectMovement.cs
+++ Assets/Examples/FallingFood_2D/ObjectMovement.cs
@@ -1,18 +1,19 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class ObjectMovement : MonoBehaviour
 {
+    [SerializeField] private float fallSpeed = 0.005f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
-    // Update is called once per frame
+    /// <summary>
+    /// Метод передвижения тела с ускорением свободного падения
+    /// </summary>
     void Update()
     {
-        
+        transform.Translate(Vector3.down * fallSpeed, Space.World);
     }
 }
