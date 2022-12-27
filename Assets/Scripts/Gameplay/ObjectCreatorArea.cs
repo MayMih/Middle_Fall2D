17e5522034411bac7494441cfb3e4a02e8037f00
Mikhail M <mayurovmih@gmail.com>
2022-12-27 21:23:24 +0300

    Расширил зону генерации объектов.

--- Assets/Scripts/Gameplay/ObjectCreatorArea.cs
+++ Assets/Scripts/Gameplay/ObjectCreatorArea.cs
@@ -18,11 +18,13 @@ public class ObjectCreatorArea : MonoBehaviour
 
 	private BoxCollider2D boxCollider2D;
 
+	private float partOfSpawnAreaWidth;
+
 	void Start ()
 	{
 		boxCollider2D = GetComponent<BoxCollider2D>();
-
-		StartCoroutine(SpawnObject());
+        partOfSpawnAreaWidth = boxCollider2D.size.x / 3;
+        StartCoroutine(SpawnObject());
 	}
 	
 	// This will spawn an object, and then wait some time, then spawn another...
@@ -31,15 +33,22 @@ public class ObjectCreatorArea : MonoBehaviour
 		while(true)
 		{
 			// Create some random numbers
-			float randomX = Random.Range (-boxCollider2D.size.x, boxCollider2D.size.x) *.5f;
-			float randomY = Random.Range (-boxCollider2D.size.y, boxCollider2D.size.y) *.5f;
+			//float randomX = Random.Range(-boxCollider2D.size.x, boxCollider2D.size.x);// *.5f;
+			//float randomY = Random.Range(-boxCollider2D.size.y, boxCollider2D.size.y);// *.5f;
 
 			// Generate the new object
 			GameObject newObject = Instantiate<GameObject>(prefabToSpawn);
-			newObject.transform.position = new Vector2(randomX + this.transform.position.x, randomY + this.transform.position.y);
-
-			// Wait for some time before spawning another object
-			yield return new WaitForSeconds(spawnInterval);
+            //newObject.transform.position = new Vector2(randomX + transform.position.x - tenPercentOfSpawnAreaWidth, 
+            //	 randomY + transform.position.y);
+            float randomX = transform.position.x + Random.Range(partOfSpawnAreaWidth - boxCollider2D.size.x, 
+				  boxCollider2D.size.x - partOfSpawnAreaWidth);
+            float randomY = Random.Range(-boxCollider2D.size.y, boxCollider2D.size.y);
+            //        newObject.transform.position = new Vector2(Mathf.Min(transform.position.x + boxCollider2D.size.x - 
+            //partOfSpawnAreaWidth, Mathf.Max(transform.position.x + partOfSpawnAreaWidth, randomX)),
+            //             randomY + transform.position.y);
+            newObject.transform.position = new Vector2(randomX,randomY + transform.position.y);
+            // Wait for some time before spawning another object
+            yield return new WaitForSeconds(spawnInterval);
 		}
 	}
 }
