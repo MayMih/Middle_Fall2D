09f15be451538015b6b6924cdeefef074a42df71
Mikhail M <mayurovmih@gmail.com>
2022-12-28 23:26:48 +0300

    Убраны закомменченные строки.

--- Assets/Examples/FallingThings_2D/Scripts/ObjectMovement.cs
+++ Assets/Examples/FallingThings_2D/Scripts/ObjectMovement.cs
@@ -2,17 +2,14 @@ using UnityEngine;
 
 public class ObjectMovement : MonoBehaviour
 {
-    //[SerializeField] private float fallSpeed = 0.005f;
-
     public float FallSpeed { get; set; }
 
-    //[SerializeField] private ObjectCreatorArea speedSource;
-
     /// <summary>
-    /// Метод передвижения тела с ускорением свободного падения
+    /// Метод передвижения тела с постоянной скоростью
     /// </summary>
     void Update()
     {
         transform.Translate(Vector3.down * FallSpeed, Space.World);
     }
+
 }
